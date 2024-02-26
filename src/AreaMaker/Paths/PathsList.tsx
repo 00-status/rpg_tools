f2b8b1a76104f30eaf353d699f60c5179182c1a1
00-status <dedypips@gmail.com>
2024-02-26 14:57:49 -0600

    Added styling to the paths

--- src/AreaMaker/Paths/PathsList.tsx
+++ src/AreaMaker/Paths/PathsList.tsx
@@ -1,4 +1,6 @@
 import { ReactElement } from "react";
+
+import './path-list.css';
 import { Path } from "../domain/types";
 
 type Props = {
@@ -30,53 +32,47 @@ export const PathsList = (props: Props): ReactElement => {
         );
     };
 
-    return <div className="area-maker--form-stack">
+    return <div className="path-list">
         <h2>Paths</h2>
         {paths.map((path: Path, index: number) => {
-            return <div key={path.id}>
-                <div className="area-maker--form-stack">
-                    <label htmlFor="path-condition-id">Path Condition ID</label>
-                    <input
-                        type="text"
-                        id="path-condition-id"
-                        onChange={(value) => {
-                            const newValue = value.target.value ?? '';
-                            const newPath: Path = { ...path, conditionID: newValue };
+            return <div className="path-list-item" key={path.id}>
+                <input
+                    placeholder="Path Condition ID"
+                    type="text"
+                    id="path-condition-id"
+                    onChange={(value) => {
+                        const newValue = value.target.value ?? '';
+                        const newPath: Path = { ...path, conditionID: newValue };
 
-                            onChange(newPath, index);
-                        }}
-                        value={path.conditionID ?? ''}
-                    />
-                </div>
-                <div className="area-maker--form-stack">
-                    <label htmlFor="path-next-area-id">Path Next-area ID</label>
-                    <input
-                        type="text"
-                        id="path-next-area-id"
-                        onChange={(value) => {
-                            const newValue = value.target.value ?? '';
-                            const newPath: Path = { ...path, nextAreaID: newValue };
-                            
-                            onChange(newPath, index);
-                        }}
-                        value={path.nextAreaID}
-                    />
-                </div>
-                <div className="area-maker--form-stack">                    
-                    <label htmlFor="path-short-description">Path Short Description</label>
-                    <input
-                        type='text'
-                        id="path-short-description"
-                        onChange={(value) => {
-                            const newValue = value.target.value ?? '';
-                            const newPath: Path = { ...path, shortDescription: newValue };
-                            
-                            onChange(newPath, index);
-                        }}
-                        value={path.shortDescription}
-                    />
-                </div>
-                <button onClick={() => onChange(null, index)}>Delete path</button>
+                        onChange(newPath, index);
+                    }}
+                    value={path.conditionID ?? ''}
+                />
+                <input
+                    type="text"
+                    placeholder="Path Next-area ID"
+                    id="path-next-area-id"
+                    onChange={(value) => {
+                        const newValue = value.target.value ?? '';
+                        const newPath: Path = { ...path, nextAreaID: newValue };
+                        
+                        onChange(newPath, index);
+                    }}
+                    value={path.nextAreaID}
+                />
+                <input
+                    type='text'
+                    placeholder="Path Short Description"
+                    id="path-short-description"
+                    onChange={(value) => {
+                        const newValue = value.target.value ?? '';
+                        const newPath: Path = { ...path, shortDescription: newValue };
+                        
+                        onChange(newPath, index);
+                    }}
+                    value={path.shortDescription}
+                />
+                <button className="delete-button" onClick={() => onChange(null, index)}>Delete path</button>
             </div>;
         })}
         <hr className="divider" />
