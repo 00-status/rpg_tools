d4e2f18927fc9d6e08375fb1725b08b6119a6813
00-status <dedypips@gmail.com>
2024-02-28 15:55:38 -0600

    Added a way to get paths from areas.

--- src/AreaMaker/domain/graphUtil.test.ts
+++ src/AreaMaker/domain/graphUtil.test.ts
@@ -1,4 +1,5 @@
-import { convertAreasToNodes } from "./graphUtil";
+import { EdgeEntry } from "graphology-types";
+import { convertAreasToEdges, convertAreasToNodes } from "./graphUtil";
 import { Area } from "./types";
 
 describe('graphUtil', () => {
@@ -39,6 +40,80 @@ describe('graphUtil', () => {
 
         it('should return an empty array when areas is empty', () => {
             const result = convertAreasToNodes([]);
+            expect(result).toHaveLength(0);
+        });
+    });
+
+    describe('convertAreasToEdges', () => {
+        it('should return an edge for each path on a node.', () => {
+            const areas: Array<Area> = [
+                {
+                    id: 'area_1',
+                    name: "Area one",
+                    description: 'Description one',
+                    paths: [
+                        {
+                            id: 'id_1',
+                            conditionID: '',
+                            nextAreaID: 'area_2',
+                            shortDescription: 'description 1',
+                        },
+                        {
+                            id: 'id_2',
+                            conditionID: '',
+                            nextAreaID: 'area_3',
+                            shortDescription: 'description 2',
+                        },
+                    ],
+                    hiddenInfo: [],
+                    pointsOfInterest: [],
+                }
+            ];
+
+            const result = convertAreasToEdges(areas);
+
+            const expected: Array<EdgeEntry> = [
+                {
+                    undirected: false,
+                    edge: 'area_1-area_2',
+                    source: 'area_1',
+                    target: 'area_2',
+                    sourceAttributes: {},
+                    targetAttributes: {},
+                    attributes: [ { label: 'description 1' } ]
+                },
+                {
+                    undirected: false,
+                    edge: 'area_1-area_3',
+                    source: 'area_1',
+                    target: 'area_3',
+                    sourceAttributes: {},
+                    targetAttributes: {},
+                    attributes: [ { label: 'description 2' } ]
+                },
+            ];
+            expect(result).toEqual(expected);
+        });
+
+        it('should return an empty array when areas is empty.', () => {
+            const result = convertAreasToEdges([]);
+            expect(result).toHaveLength(0);
+        });
+
+        it('should return an empty array when each area has no paths.', () => {
+            const areas: Array<Area> = [
+                {
+                    id: 'area_1',
+                    name: "Area one",
+                    description: 'Description one',
+                    paths: [],
+                    hiddenInfo: [],
+                    pointsOfInterest: [],
+                }
+            ];
+
+            const result = convertAreasToEdges(areas);
+
             expect(result).toHaveLength(0);
         });
     });
--- src/AreaMaker/domain/graphUtil.ts
+++ src/AreaMaker/domain/graphUtil.ts
@@ -1,4 +1,4 @@
-import { NodeEntry, SerializedNode } from "graphology-types";
+import { EdgeEntry, NodeEntry, SerializedNode } from "graphology-types";
 import { Area } from "./types";
 
 export const convertAreasToNodes = (areas: Array<Area>): Array<NodeEntry> => {
@@ -10,4 +10,29 @@ export const convertAreasToNodes = (areas: Array<Area>): Array<NodeEntry> => {
     });
 
     return nodes;
+};
+
+export const convertAreasToEdges = (areas: Array<Area>): Array<EdgeEntry> => {
+    // graph.addEdgeWithKey("rel1", "A", "B", { label: "REL_1" });
+
+    // For each path in an area.
+    //      Create an edge with the source node being the area_id and the target node being the nextAreaId
+
+    const mappedEdges = areas.reduce<Array<EdgeEntry>>((acc, area) => {
+        const edges: Array<EdgeEntry> = area.paths.map((path) => {
+            return { 
+                undirected: false,
+                edge: area.id + '-' + path.nextAreaID,
+                source: area.id,
+                target: path.nextAreaID,
+                sourceAttributes: {},
+                targetAttributes: {},
+                attributes: [{ label: path.shortDescription }]
+             };
+        });
+
+        return [...acc, ...edges];
+    }, []);
+
+    return mappedEdges;
 };
