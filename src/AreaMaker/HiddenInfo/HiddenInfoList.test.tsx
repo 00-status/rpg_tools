458508ce6900c4f81b774af741257f43d8343ff5
00-status <dedypips@gmail.com>
2024-03-18 12:24:14 -0600

    Updated tests

--- src/AreaMaker/HiddenInfo/HiddenInfoList.test.tsx
+++ src/AreaMaker/HiddenInfo/HiddenInfoList.test.tsx
@@ -1,43 +1,41 @@
 import { render } from "@testing-library/react";
 import { HiddenInfoList } from "./HiddenInfoList";
 import userEvent from "@testing-library/user-event";
+import { HiddenInfo } from "../domain/types";
 
 describe('HiddenInfoList', () => {
     it('should render a "Create new" button when there are no Hidden Infos created yet.', () => {
-        const { getByText } = render(<HiddenInfoList onSave={jest.fn()} />);
+        const { getByText } = render(<HiddenInfoList hiddenInfos={[]} setHiddenInfos={jest.fn()} />);
 
         getByText('Hidden Info');
         getByText('Create new');
     });
 
     it('should render a form after pressing the "create new" Button.', async () => {
-        const { getByText, getByLabelText } = render(<HiddenInfoList onSave={jest.fn()} />);
+        const onSaveMock = jest.fn();
+        const { getByText } = render(<HiddenInfoList hiddenInfos={[]} setHiddenInfos={onSaveMock} />);
 
+        expect(onSaveMock).toHaveBeenCalledTimes(0);
         await userEvent.click(getByText('Create new'));
+        expect(onSaveMock).toHaveBeenCalledTimes(1);
+    });
+
+    it('should render the passed in hidden info array as a form', async () => {
+        const hiddenInfos: Array<HiddenInfo> = [
+            {
+                id: 'id_1',
+                conditionIDs: ['condition_1', 'condition_2'],
+                description: 'descripting_things'
+            }
+        ];
+        const { getByLabelText, getByDisplayValue } =render(
+            <HiddenInfoList hiddenInfos={hiddenInfos} setHiddenInfos={jest.fn()} />
+        );
 
         getByLabelText('Hidden Info Condition IDs');
         getByLabelText('Hidden Info Description');
-    });
 
-    it('should call onSave when saving form', async () => {
-        const onSaveMock = jest.fn();
-        const { getByLabelText, getByText } = render(<HiddenInfoList onSave={onSaveMock} />);
-
-        await userEvent.click(getByText('Create new'));
-
-        await userEvent.type(getByLabelText('Hidden Info Condition IDs'), 'skill_magic,skill_brawn');
-        await userEvent.type(getByLabelText('Hidden Info Description'), 'The fox jumped over the badger-bear.');
-
-        expect(onSaveMock).toHaveBeenCalledTimes(0);
-        await userEvent.click(getByText('Save'));
-        expect(onSaveMock).toHaveBeenCalledTimes(1);
-        expect(onSaveMock).toHaveBeenCalledWith([
-            expect.objectContaining(
-                {
-                    conditionIDs: ['skill_magic', 'skill_brawn'],
-                    description: 'The fox jumped over the badger-bear.'
-                }
-            )
-        ]);
+        getByDisplayValue('condition_1,condition_2');
+        getByDisplayValue('descripting_things');
     });
 });
